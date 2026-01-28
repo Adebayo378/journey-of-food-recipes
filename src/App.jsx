7dcf148a36b318656a90280d28cb9df66ec4c639
Daniel <idowudanieladebayo787@gmail.com>
2026-01-28 12:40:26 -0800

    welcome to food recipes

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div>
+      <h1 className=" public text-2xl font-bold text-red-600">
+        WELCOME TO My Food Recipe
+      </h1>
+    </div>
+  );
+}
